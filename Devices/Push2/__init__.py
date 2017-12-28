8016d7b4c3159cd6c3d179e0958df9c2ed0f4ace
Paul Hnaley <hanleyp@gmail.com>
2017-12-27 22:54:46 -0500

    Sync’ed Push2 __init__.py with Ableton 10.0.1b5

--- Devices/Push2/__init__.py
+++ Devices/Push2/__init__.py
@@ -1,20 +1,18 @@
 # Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Push2/__init__.py
-from __future__ import absolute_import, print_function
-
-from Ubermap import UbermapDevicesPatches
+from __future__ import absolute_import, print_function, unicode_literals
 
 def get_capabilities():
     from ableton.v2.control_surface import capabilities as caps
-    return {caps.CONTROLLER_ID_KEY: caps.controller_id(vendor_id=10626, product_ids=[6503], model_name='Ableton Push 2'),
-     caps.PORTS_KEY: [caps.inport(props=[caps.HIDDEN, caps.NOTES_CC, caps.SCRIPT]),
+    return {caps.CONTROLLER_ID_KEY: caps.controller_id(vendor_id=10626, product_ids=[
+                              6503], model_name=u'Ableton Push 2'),
+       caps.PORTS_KEY: [
+                      caps.inport(props=[caps.HIDDEN, caps.NOTES_CC, caps.SCRIPT]),
                       caps.inport(props=[]),
-                      caps.outport(props=[caps.HIDDEN,
-                       caps.NOTES_CC,
-                       caps.SYNC,
-                       caps.SCRIPT]),
+                      caps.outport(props=[caps.HIDDEN, caps.NOTES_CC, caps.SYNC, caps.SCRIPT]),
                       caps.outport(props=[])],
-     caps.TYPE_KEY: 'push2',
-     caps.AUTO_LOAD_KEY: True}
+       caps.TYPE_KEY: u'push2',
+       caps.AUTO_LOAD_KEY: True
+       }
 
 
 def create_instance(c_instance):
@@ -22,6 +20,6 @@ def create_instance(c_instance):
     from .push2_model import Root, Sender
     root = Root(sender=Sender(message_sink=c_instance.send_model_update, process_connected=c_instance.process_connected))
 
-    UbermapDevicesPatches.apply_ubermap_patches()
-
+    from Ubermap import UbermapDevicesPatches
+    UbermapDevicesPatches.apply_ubermap_patches(False)
     return Push2(c_instance=c_instance, model=root)
